184c8a4920927fdccb231fb5461ac9e6c59cdd9e
Lucas Yuji S. Aciole <lucas.aciole@gmail.com>
2016-08-23 16:53:46 -0300

    Add BootStrapping domain objects

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,33 @@
+import br.ufscar.dc.dsw.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        def sp = new Estado(sigla: "SP", nome: "São Paulo")
+        sp.save()
+        if (sp.hasErrors()) {
+            println sp.errors
+        }
+
+        println 'Populando estados - OK'
+
+        def sanca = new Cidade(nome: "São Carlos", estado: sp)
+        sanca.save()
+        if(sanca.hasErrors()) {
+            println sanca.errors
+        }
+
+        def sampa = new Cidade(nome: "São Paulo", estado: sp)
+        sampa.save()
+        if(sampa.hasErrors()) {
+            println sampa.errors
+        }
+
+        println "Populando cidades - OK"
+
+        def end1 = new Endereco()
     }
+
     def destroy = {
     }
 }
